cf85eafa35f7f4d4c84caaac72b3bebadb203825
will <jaskamiin@gmail.com>
2017-05-26 22:16:30 -0400

    add counter

--- Roll a Ball/Assets/Scripts/PlayerController.cs
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
-	public float speed = 1;
+	public float speed;
+	private int score;
 
+	public Text countText;
 	private Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
-		rb = GetComponent<Rigidbody>();
+		rb 	  = GetComponent<Rigidbody>();
+		score = 0;
+		UpdateCountText ();
 	}
 	
 	// Update is called once per frame
@@ -29,6 +34,12 @@ public class PlayerController : MonoBehaviour {
 	void OnTriggerEnter(Collider other) {
 		if (other.gameObject.CompareTag("Pick Up")) {
 			other.gameObject.SetActive (false);
+			score++;
+			UpdateCountText ();
 		}
+	}
+
+	void UpdateCountText(){
+		countText.text = "Count: " + score.ToString();
 	}
 }
