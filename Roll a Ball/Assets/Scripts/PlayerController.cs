96f76f8e6033046ef5c610a1349e05dc23c1b694
will <jaskamiin@gmail.com>
2017-05-26 21:58:53 -0400

    add pickable objects, add triggers, etc

--- Roll a Ball/Assets/Scripts/PlayerController.cs
+++ Roll a Ball/Assets/Scripts/PlayerController.cs
@@ -7,7 +7,6 @@ public class PlayerController : MonoBehaviour {
 	public float speed = 1;
 
 	private Rigidbody rb;
-	
 
 	// Use this for initialization
 	void Start () {
@@ -24,7 +23,12 @@ public class PlayerController : MonoBehaviour {
 		float moveHorizontal = Input.GetAxis("Horizontal");
 		float moveVertical = Input.GetAxis("Vertical");
 	
-		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-		rb.AddForce(movement * speed);
+		rb.AddForce(new Vector3(moveHorizontal, 0.0f, moveVertical) * speed);
     }
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag("Pick Up")) {
+			other.gameObject.SetActive (false);
+		}
+	}
 }
