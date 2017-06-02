419607291727e4c511510fe188b50bb9981df397
will <jaskamiin@gmail.com>
2017-06-02 00:00:27 -0400

    add continuous waves (with infinite loop)

--- Space Shooter/Assets/Assets/Scripts/GameController.cs
+++ Space Shooter/Assets/Assets/Scripts/GameController.cs
@@ -5,20 +5,27 @@ using UnityEngine;
 public class GameController : MonoBehaviour {
 
 	public GameObject hazard;
+	public Vector3 spawnValue;
+	public float spawnWait;
+	public float startWait;
+	public int hazardCount;
 
-	void SpawnWaves(){
-		Vector3 spawnPosition = new Vector3 ();
-		Quaternion spawnRotation = new Quaternion ();
-		Instantiate (hazard, spawnPosition, spawnRotation);
-	}
 
 	// Use this for initialization
 	void Start () {
-		SpawnWaves ();	
+		StartCoroutine (SpawnWaves ());
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	IEnumerator SpawnWaves(){
+		yield return new WaitForSeconds (startWait);
+		while(true){
+			for(int i = 0; i < hazardCount; ++i){
+				Vector3 spawnPosition = new Vector3 (Random.Range(-spawnValue.x,spawnValue.x),spawnValue.y, spawnValue.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate (hazard, spawnPosition, spawnRotation);
+				yield return new WaitForSeconds (Random.Range(0, spawnWait));
+			}
+		}
 	}
+
 }
