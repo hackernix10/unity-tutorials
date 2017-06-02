a8f9c757b89b637d16723bb9d1887bf89ba15008
will <jaskamiin@gmail.com>
2017-06-02 00:48:24 -0400

    add rudimentary z-axis velocity change for asteroids

--- Space Shooter/Assets/Assets/Scripts/GameController.cs
+++ Space Shooter/Assets/Assets/Scripts/GameController.cs
@@ -20,8 +20,11 @@ public class GameController : MonoBehaviour {
 	IEnumerator SpawnWaves(){
 		yield return new WaitForSeconds (startWait);
 		for(int waveNumber = 1; waveNumber <= 20; waveNumber++, hazardCount += 5){
+			hazard.GetComponent<Rigidbody> ().velocity = hazard.transform.forward * (10 + Mathf.Log10 (waveNumber));
 			Debug.Log ("Wave " + waveNumber);
 			Debug.Log ("Hazards " + hazardCount);
+			Debug.Log ("Velocity " + hazard.GetComponent<Rigidbody> ().velocity);
+
 			for(int i = 0; i < hazardCount; ++i){
 				Vector3 spawnPosition = new Vector3 (Random.Range(-spawnValue.x,spawnValue.x),spawnValue.y, spawnValue.z);
 				Quaternion spawnRotation = Quaternion.identity;
